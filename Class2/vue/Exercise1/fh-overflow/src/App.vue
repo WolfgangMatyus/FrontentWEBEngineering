493fbe9c05790b73b6b0480cf239a4812268f81f
Matyus Wolfgang <wi21b055@technikum-wien.at>
2023-10-04 20:33:02 +0200

    vue cli

--- Class2/vue/Exercise1/fh-overflow/src/App.vue
+++ Class2/vue/Exercise1/fh-overflow/src/App.vue
@@ -1,18 +1,9 @@
 <template>
-<Hero
-  headingText="The IKEA family card"
-  text="So start getting discounts!"
-  backgroundColor="blue" 
-  />
-  <Hero
-  headingText="The IKEA family card"
-  text="So, you think you're a fan?"
-  backgroundColor="yellow" 
-  />
+
 
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 import Hero from './components/Hero.vue'
+
 </script>
--- Class2/vue/Exercise1/fh-overflow/src/components/Hero.vue
+++ Class2/vue/Exercise1/fh-overflow/src/components/Hero.vue
@@ -1,46 +1,21 @@
 <template>
-
-<a class="drop-container text-bright svelte-c8hygj" style="background-color: rgb(0, 90, 166);" href="https://www.ikea.com/global/en/stories/design/ikea-icons-2023/">
-    <div class="drop-image-container svelte-c8hygj">
-    <img style="object-position: 50% 50%;" alt="A red FROSET chair." loading="lazy" width="1500" height="1500" src="https://www.ikea.com/global/en/images/FROSET_8aab389a69.jpg" 
-    srcset="https://www.ikea.com/global/en/images/FROSET_8aab389a69.jpg?f=g 2000w, 
-    https://www.ikea.com/global/en/images/FROSET_8aab389a69.jpg?f=sg 1600w, 
-    https://www.ikea.com/global/en/images/FROSET_8aab389a69.jpg?f=xxxl 1400w, 
-    https://www.ikea.com/global/en/images/FROSET_8aab389a69.jpg?f=xxl 1100w, 
-    https://www.ikea.com/global/en/images/FROSET_8aab389a69.jpg?f=xl 900w, 
-    https://www.ikea.com/global/en/images/FROSET_8aab389a69.jpg?f=l 750w, 
-    https://www.ikea.com/global/en/images/FROSET_8aab389a69.jpg?f=m 700w, 
-    https://www.ikea.com/global/en/images/FROSET_8aab389a69.jpg?f=s 600w, 
-    https://www.ikea.com/global/en/images/FROSET_8aab389a69.jpg?f=xs 500w, 
-    https://www.ikea.com/global/en/images/FROSET_8aab389a69.jpg?f=xxs 400w, 
-    https://www.ikea.com/global/en/images/FROSET_8aab389a69.jpg?f=xxxs 300w, #
-    https://www.ikea.com/global/en/images/FROSET_8aab389a69.jpg?f=u 160w, #
-    https://www.ikea.com/global/en/images/FROSET_8aab389a69.jpg?f=xu 80w" 
-    sizes="(min-width: 1440px) 686px, 45vw" class="svelte-1ujc5pi"> 
-</div> 
-<div class="content svelte-c8hygj">
-<div class="copy-container svelte-c8hygj">
-<div class="arrow-container svelte-c8hygj">
-<svg width="48" height="48" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" class="svelte-1iyybxb right">
-<path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M4,12l8,8,1.41-1.41L7.83,13H20V11H7.83l5.59-5.59L12,4Z">
-</path>
-</svg>
-</div> <div class="title-container svelte-c8hygj">
-<p class="svelte-c8hygj">So, you think you're a fan?</p> 
-<h2 style="max-width: 10ch;" class="h-l no-spacing small-mobile x-small-mobile svelte-8im5kj">The IKEA icons quiz!</h2>
-</div>
-</div>
-</div>
-</a>
-
+    <div :style="{ backgroundColor: backgroundColor }">
+        <h2>{{ title }}</h2>
+        <p>{{ text }}</p>
+    </div>
 </template>
 
-
 <script>
-export default {
-    name: "Hero"
-};
+    export default {
+        name: 'Hero',
+        props: {
+            title: String,
+            text: String,
+            backgroundColor: String,
+        },
+    };
 </script>
 
-<style>
+<style scoped>
+
 </style>

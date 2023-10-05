bfe5175cee8692de08c889115424dc969f525cd8
Matyus Wolfgang <wi21b055@technikum-wien.at>
2023-10-05 19:52:26 +0200

    installed axios

--- Class2/vue/w3/src/App.vue
+++ Class2/vue/w3/src/App.vue
@@ -1,15 +1,35 @@
 <template>
-  <div id="app">
-    <RegistrierungsFormular />
-  </div>
+  <h1>Food</h1>
+  <p>My favorite food has a diploma image attached to it.</p>
+  <div id="wrapper">
+    <food-item 
+      food-name="Apples" 
+      food-desc="Apples are a type of fruit that grow on trees."
+      v-bind:is-favorite="false"/>
+    <food-item 
+      food-name="Pizza"
+      food-desc="Pizza has a bread base with tomato sauce, cheese, and toppings on top."
+      v-bind:is-favorite="true"/>
+    <food-item 
+      food-name="Rice"
+      v-bind:is-favorite="false"/><!--food-desc="Rice is a type of grain that people like to eat."-->
+  </div> 
 </template>
 
-<script>
-import RegistrierungsFormular from './components/RegistrierungsFormular.vue';
+<script></script>
 
-export default {
-  components: {
-    RegistrierungsFormular
+<style>
+  #wrapper {
+    display: flex;
+    flex-wrap: wrap;
   }
-};
-</script>
+  #wrapper > div {
+    border: dashed black 1px;
+    flex-basis: 120px;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+</style>                  
+
+
